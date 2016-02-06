7a512a517d1061ee03da4d3d86b9bba0179ee57e
bnangue <bricenangue@gmail.com>
2016-02-06 20:44:24 +0100

    background success

---
+++ app/src/main/java/com/bricefamily/alex/time_tracker/GCMessageHandler.java
@@ -0,0 +1,96 @@
+package com.bricefamily.alex.time_tracker;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+/**
+ * Created by bricenangue on 06/02/16.
+ */
+public class GCMessageHandler extends IntentService {
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+
+    Notification noti;
+    NotificationManager nmgr;
+    public static final int NOTIFICATION_ID = 0;
+
+    NotificationCompat.Builder mBuilder;
+    String mes;
+    private Handler handler;
+    public GCMessageHandler() {
+        super("GcmMessageHandler");
+    }
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+        handler = new Handler();
+        nmgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.colornfo)
+                        .setContentTitle(mes)
+                        .setContentText(mes);
+        Intent intent = new Intent(this,CentralPageActivity.class);
+        // The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(CentralPageActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(intent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        mes = extras.getString("title");
+        mBuilder.setContentText(mes);
+        showToast();
+        Log.i("GCM", "Received : (" + messageType + ")  " + extras.getString("title"));
+
+        GCMBroadcastReceiver.completeWakefulIntent(intent);
+
+    }
+
+    public void showToast(){
+        handler.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), mes, Toast.LENGTH_LONG).show();
+                nmgr.notify(NOTIFICATION_ID, mBuilder.build());
+
+            }
+        });
+
+    }
+}

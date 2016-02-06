1da19b25c965277c1e0806db9c6b8631edf8a58a
bnangue <bricenangue@gmail.com>
2016-02-06 03:39:22 +0100

    background color

---
+++ app/src/main/java/com/bricefamily/alex/time_tracker/UserListActivity.java
@@ -0,0 +1,160 @@
+package com.bricefamily.alex.time_tracker;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserListActivity extends AppCompatActivity implements AdapterView.OnItemLongClickListener, AdapterView.OnItemClickListener {
+    ArrayList<User> userArrayList;
+    ProfileListAdapter profileListAdapter;
+
+    int[] status ;
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        prepareView();
+       Bundle extras=getIntent().getExtras();
+        if(extras!=null){
+            userArrayList=extras.getParcelableArrayList("userlist");
+        }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        status=setstatuslist(userArrayList);
+
+       prepareListview(userArrayList,status);
+
+    }
+
+    public void prepareView() {
+
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE); //Hintergrund der View
+
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+
+        //Disablen des Zurück Pfeils
+        if (findViewById(android.R.id.home) != null) {
+            findViewById(android.R.id.home).setVisibility(View.GONE);
+        }
+
+        LayoutInflater inflator = (LayoutInflater) getSystemService(getApplicationContext().LAYOUT_INFLATER_SERVICE);
+        View view = inflator.inflate(R.layout.actionbarbackground, null);
+
+
+        //center des ActionBar Titles
+        android.support.v7.app.ActionBar.LayoutParams params = new android.support.v7.app.ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+
+        try {
+            ab.setDisplayShowCustomEnabled(true);
+            ab.setDisplayShowTitleEnabled(false);
+            ab.setCustomView(view, params);
+            ab.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.cellSelected)));
+        } catch (NullPointerException e) {
+            Log.w("ActionBar Error", e.getMessage());
+        }
+        try {
+            //ab Android 5.0
+            ab.setElevation(0);
+        } catch (NullPointerException e) {
+            Log.w("ActionBar Error", e.getMessage());
+        }
+
+    }
+
+
+    void prepareListview(ArrayList<User> list,int[] statsus){
+        listView=(ListView)findViewById(R.id.userListview);
+        profileListAdapter=new ProfileListAdapter(this,list);
+
+        listView.setAdapter(profileListAdapter);
+        profileListAdapter.setUserStatus(statsus);
+        profileListAdapter.notifyDataSetChanged();
+
+        listView.setOnItemLongClickListener(this);
+        listView.setOnItemClickListener(this);
+
+        if(status==null){
+            status=new int[list.size()];
+        }
+    }
+
+    private int[] setstatuslist(ArrayList<User> list){
+        int[] status=new int[list.size()];
+        for(int i=0;i <list.size();i++){
+            if(list.get(i).status==0){
+                status[i]=0;
+            }else {
+                status[i]=1;
+            }
+        }
+        return status;
+    }
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+       onLongListItemClick(view, position,id);
+        return true;
+    }
+    protected boolean onLongListItemClick(View v, int pos, long id) {
+        //selectionevents[pos]=true;
+        // countevent=countevent+1;
+        // selected(countevent, selectionevents);
+        // centralPageAdapter.notifyDataSetChanged();
+        showFilterPopup(v);
+        return true;
+    }
+    private void showFilterPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        // Inflate the menu from xml
+        popup.getMenuInflater().inflate(R.menu.popupmenu, popup.getMenu());
+        // Setup menu item selection
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_keyword:
+                        Toast.makeText(UserListActivity.this, "share", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menu_popularity:
+                        Toast.makeText(UserListActivity.this, "delete!", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        // Handle dismissal with: popup.setOnDismissListener(...);
+        // Show the menu
+        popup.show();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+}
